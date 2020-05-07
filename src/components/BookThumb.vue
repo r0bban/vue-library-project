9f938f1d1b83ce181ceb7fecd5cdbb9063f6096f
RobertPS <robert.p.svensson@gmail.com>
2020-05-07 17:48:26 +0200

    bookshelf design fixes

--- src/components/BookThumb.vue
+++ src/components/BookThumb.vue
@@ -1,5 +1,5 @@
 <template>  
-<article class="book-thumb" v-bind:style="{background: color}">
+<article class="book-thumb" v-bind:style="{'background-image': bgColor}">
     <div class="cover-back"></div>
     <div class="book-data-container">
         <h2 class="book-title">{{title}}</h2>
@@ -17,6 +17,11 @@ export default {
         title: String,
         author: String,
         color: String
+    },
+    computed:{
+        bgColor(){
+            return `linear-gradient(to bottom left, ${this.color}8C, ${this.color})`;
+        }
     }
 
 }
@@ -26,23 +31,29 @@ export default {
 article.book-thumb {
     display: flex;
     flex-wrap: nowrap;
-    min-width: 10rem;
+    width: 300px;
+    height: 400px;
     margin:10px;
 }
 .cover-back{
-    border-right: 3px;
-    border-color: gray;
+    border-width:0 3px 0 0;
+    border-color: rgba(98, 98, 98, 0.459);
     border-style: solid;
+    min-width: 20px;
 }
 
 .book-data-container {
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
+    flex-grow: 1;
+    padding: 10px;
 }
 
 .book-title, .book-author{
    color: white;
+   margin: 0;
+   text-align: left;
 }
 
 
--- src/views/BookShelf.vue
+++ src/views/BookShelf.vue
@@ -1,44 +1,58 @@
 <template>
   <section class="bookshelf-container">
-  <h1>8 Classic Children books</h1>
+    <h1>8 Classic Children books</h1>
     <div class="books-container">
-        <BookThumb
+      <BookThumb
         v-for="book in getBooks"
         v-bind:key="book.id"
         v-bind:id="book.id"
         v-bind:title="book.title"
         v-bind:author="book.author"
         v-bind:color="book.color"
-        />
+      />
     </div>
   </section>
 </template>
 
 <script>
-import BookThumb from "../components/BookThumb"
+import BookThumb from "../components/BookThumb";
 
 export default {
-    data(){
-        return{
-        }
-    },
-    computed: {
-        getBooks(){
-            return this.$root.booksDB
-        }
-    },
-    name: 'BookShelf',
-    components: {
+  name: "BookShelf",
+  data() {
+    return {};
+  },
+  computed: {
+    getBooks() {
+      return this.$root.booksDB;
+    }
+  },
+  components: {
     BookThumb: BookThumb
   }
-}
+};
 </script>
 
 <style scoped>
-div.books-container{
-    display: flex;
-    /* flex-direction: row; */
-    flex-wrap: wrap;
+.bookshelf-container {
+  max-width: 1300px;
+  margin: 0 auto;
+}
+h1 {
+  font-size: 3rem;
+  margin: 5px 0;
+  text-align: left;
+}
+div.books-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  padding: 0 auto;
 }
 
+@media screen and (max-width: 640px) {
+  h1 {
+    font-size: 1.6rem;
+  }
+}
 </style>
