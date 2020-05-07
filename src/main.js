4f689fffc65da4e254a4e2140b89ce1129b072e0
RobertPS <robert.p.svensson@gmail.com>
2020-05-07 14:01:11 +0200

    imported book source from jason-file as object  to main and console.logged upon click in About component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BooksSourceObject from './assets/childrensbooks.json'
 
 Vue.config.productionTip = false
 
 new Vue({
+
+
+  data(){
+    return {
+      booksDB: BooksSourceObject.books
+    }
+  },
+
   router,
   render: h => h(App)
 }).$mount('#app')
