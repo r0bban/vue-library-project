761d8d8f6261c4af91c11f29276c17aaa3b4e85d
RobertPS <robert.p.svensson@gmail.com>
2020-05-12 10:17:58 +0200

    removed unused stuff

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Bookshelf from '../views/BookShelf.vue'
 import SingleBook from '../views/SingleBook.vue'
 
@@ -16,20 +15,7 @@ Vue.use(VueRouter)
       path:'/book/id=:bookId',
       name: 'SingleBook',
       component: SingleBook
-    },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    }
 ]
 
 const router = new VueRouter({
